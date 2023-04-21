8535e6d8bb4aee996ff6ee1070c60bf1678c001d
S203932 <s203932@student.dtu.dk>
2023-04-21 09:36:09 +0200

    Addded print screen

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Please");
+
+    char *fs[] = {"F1", "F2", "F3", "F4"};
+    printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    for(int i = 0; i < 4;i++){
+        for(int j = 0; j < 7; j++){
+            printf("\t");
+        }
+        printf("[]\t%s\n",fs[i]);
+    }
+
+
+
+
     return 0;
 }
