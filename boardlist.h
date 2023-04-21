db6ab12088e469361a883d62567cf1ad3f4421db
KrisUrb <s163301@student.dtu.dk>
2023-04-21 11:12:21 +0200

    Added boardlist.h

---
+++ boardlist.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void empty_board(void){
+    char *fs[] = {"F1", "F2", "F3", "F4"};
+    printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    int counter = 0;
+    for(int i = 0; i < 7;i++){
+        for(int j = 0; j < 7; j++){
+            printf("[]\t");
+        }
+        if((i)%2 == 0){
+            printf("[]\t%s\n",fs[counter]);
+            counter++;
+        }else{
+            printf("\n");
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        printf("[]\t");
+    }
+}
+
+
+void start_board(void) {
+    char *fs[] = {"F1", "F2", "F3", "F4"};
+    printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    int counter = 0;
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            printf("\t");
+        }
+        printf("[]\t%s\n", fs[i]);
+    }
+}
--- main.c
+++ main.c
@@ -1,43 +1,9 @@
 #include <stdio.h>
-
+#include "boardlist.h"
 
 int main() {
 
-    /*
-    char *fs[] = {"F1", "F2", "F3", "F4"};
-    printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
-    int counter = 0;
-    for(int i = 0; i < 7;i++){
-        for(int j = 0; j < 7; j++){
-            printf("[]\t");
-        }
-        if((i)%2 == 0){
-            printf("[]\t%s\n",fs[counter]);
-            counter++;
-        }else{
-            printf("\n");
-        }
-    }
-    for(int i = 0; i < 3; i++){
-        printf("[]\t");
-    }
-     */
-
-
-    /*
-    char *fs[] = {"F1", "F2", "F3", "F4"};
-    printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
-    int counter = 0;
-    for(int i = 0; i < 7;i++){
-        for(int j = 0; j < 7; j++){
-            printf("\t");
-        }
-        printf("[]\t%s\n",fs[i]);
-    }
-     */
-
-
-
+empty_board();
 
     return 0;
 }
