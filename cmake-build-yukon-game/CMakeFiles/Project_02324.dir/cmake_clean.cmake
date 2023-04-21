5b232142409b1663c44e783ea883d5e11c2f5fd5
Baime0 <s215790@dtu.dk>
2023-04-21 09:14:25 +0200

    Creating empty Linkedlist

---
+++ cmake-build-yukon-game/CMakeFiles/Project_02324.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_02324.dir/linkedlist.c.obj"
+  "CMakeFiles/Project_02324.dir/linkedlist.c.obj.d"
+  "CMakeFiles/Project_02324.dir/main.c.obj"
+  "CMakeFiles/Project_02324.dir/main.c.obj.d"
+  "Project_02324.exe"
+  "Project_02324.exe.manifest"
+  "Project_02324.pdb"
+  "libProject_02324.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_02324.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linkedlist.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// start of structs//
+
+struct CardList {
+    bool faceDown;
+    bool InGame;
+    char suit;
+    char value;
+};
+
+typedef struct {
+    struct CardList* head;
+    struct CardList* tail;
+    int size;
+} Linked_list;
+
+// Start of methods//
+Linked_list *createLinkedList();
+
+void Append_of_Card(Linked_list *list, struct CardList cards);
