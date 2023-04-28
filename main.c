acc696ae19e7d20118c8f9bb7df9bc66b53cb338
KrisUrb <s163301@student.dtu.dk>
2023-04-28 10:17:16 +0200

    Adding process_input.h and main changes.

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include "boardlist.h"
 #include "ld.h"
+#include "process_input.h"
 #include <dirent.h>
 #include<unistd.h>
-/*
+
 int main() {
-
-//empty_board();
-
-printf(LD(" "));
-
-
-
-return 0;
+    empty_board();
+    char *m2 = LD(" ");
+    char input2[10];
+    strcpy(input2, process_input(m2));
+    printf("%s", input2);
+    return 0;
 }
-*/
---
+++ process_input.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+char* process_input(char* message) {
+    char last_cmd[10] = "";
+    char* input = malloc(sizeof(char) * 10);
+    printf("\nLAST Command: %s\n", last_cmd);
+    printf("Message: %s \n",message);
+    printf("INPUT > ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0'; // remove trailing newline
+
+    return input;
+
+
+    /*while (1) {
+
+
+        if (strcmp(input, "QQ") == 0) {
+            printf("Exiting without saving!\n");
+            break;
+        }
+        else if (strcmp(input, "Q") == 0) {
+            printf("Saving and exiting...\n");
+            // Save the game here
+            break;
+        }
+
+        // Process the input command here
+        printf("ok\n");
+
+        strcpy(last_cmd, input);
+    }*/
+}
